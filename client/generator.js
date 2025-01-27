64b3ec79771e1bbe67c0b5faf44873ef416a877c
Hilda Khajehamiri <hkhajehamiri@usertesting.com>
2025-01-27 23:14:18 +0400

    Update generator to only handle creating random emojis - Add recursive generation to keep trying for when the api cannot combine the two emojis - Update with default emoji values & params to use in scraper

--- client/generator.js
+++ client/generator.js
@@ -1,57 +1,16 @@
-import * as dotenv from 'dotenv';
-dotenv.config({ path: '../.env' });
-
 import { emojiList } from './emojiList.js';
-import { Client } from '@notionhq/client';
 
-const notion = new Client({ auth: process.env.NOTION_KEY })
+export const generateRandomEmoji = async (
+  randomEmoji1 = emojiList[Math.floor(Math.random() * emojiList.length)],
+  randomEmoji2 = emojiList[Math.floor(Math.random() * emojiList.length)]
+) => {
+  const response = await fetch(
+    `https://emojik.vercel.app/s/${encodeURIComponent(randomEmoji1)}_${encodeURIComponent(randomEmoji2)}?size=128`
+  )
 
-export const createNotionPageWithEmoji = async (title, content) => {
-  try {
-    const randomEmoji1 = emojiList[Math.floor(Math.random() * emojiList.length)]
-    const randomEmoji2 = emojiList[Math.floor(Math.random() * emojiList.length)]
-
-    const response = await fetch(
-      `https://emojik.vercel.app/s/${encodeURIComponent(randomEmoji1)}_${encodeURIComponent(randomEmoji2)}?size=128`
-    )
-    const { url } = await response;
-
-    // Create page with emoji as icon
-    return await notion.pages.create({
-      parent: { 
-        page_id: process.env.NOTION_PARENT_PAGE_ID,
-       },
-      icon: {
-        type: "external",
-        external: { url }
-      },
-      properties: {
-        title: { title: [{ text: { content: title } }] },
-      },
-      children: [
-        {
-          object: 'block',
-          type: 'paragraph',
-          paragraph: {
-            rich_text: [
-              {
-                type: 'text',
-                text: {
-                  content,
-                },
-              },
-            ],
-          },
-        },
-      ],
-    })
-  } catch (err) {
-    console.error(err)
-    return null
+  if (!response.ok || response.headers.get('status') === '404') {
+    return generateRandomEmoji();
   }
-}
 
-await createNotionPageWithEmoji(
-  'new Emoji page', 
-  'You made this page using the Notion API. Pretty cool, huh? We hope you enjoy building with us.'
-);
+  return await response;
+};
